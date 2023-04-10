f5c60b31054bb6e32fb622e650bfe6cda64e1182
Radu Dobrinescu <radu.dobrinescu@gmail.com>
2023-04-10 17:20:26 +0300

    initial mng size to 3

--- examples/gitops/argocd/main.tf
+++ examples/gitops/argocd/main.tf
@@ -61,8 +61,8 @@ module "eks_blueprints" {
       instance_types  = ["t3.large"]
       subnet_ids      = module.vpc.private_subnets
 
-      desired_size = 2
-      max_size     = 2
+      desired_size = 3
+      max_size     = 3
       min_size     = 1
     }
   }
